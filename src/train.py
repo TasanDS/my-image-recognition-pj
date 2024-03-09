374eaec57c98dc2da0281704007e831fe20ddba7
TasanDS <taishi.datascience@gmail.com>
2024-03-09 10:12:08 +0900

    modify train.py which had raised at the part of early stopping

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 import torch
 from tqdm import tqdm
 
-def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs, early_stopping=None, save_path=None):
+def train_model(net, dataloaders_dict, optimizer, criterion, num_epochs, early_stopping=None, save_path=None):
     """
     function for training model
 
@@ -29,11 +29,11 @@ def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs, early_s
     net.to(device)
     torch.backends.cudnn.benchmark = True
 
-    # lists for logging
-    train_loss_hist = []
-    train_corrects_hist = []
-    val_loss_hist = []
-    val_corrects_hist = []
+    # lists for storing logging
+    train_losses = []
+    train_accuracies = []
+    val_losses = []
+    val_accuracies = []
 
     best_val_loss = float('inf')
     no_improve_cnt = 0
@@ -53,19 +53,28 @@ def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs, early_s
             epoch_corrects = 0
     
             for inputs, labels in tqdm(dataloaders_dict[phase]):
-
                 inputs = inputs.to(device)
                 labels = labels.to(device)
                 optimizer.zero_grad()
-    
-                with torch.set_grad_enabled(phase=='train'):
+                
+                if phase == 'train':
+                    # forward
                     outputs = net(inputs)
                     loss = criterion(outputs, labels)
                     _, preds = torch.max(outputs, 1)
-    
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+
+                    # backward
+                    loss.backward()
+                    optimizer.step()
+                    
+                elif phase == 'val':
+                    with torch.no_grad():
+                        outputs = net(inputs)
+                        loss = criterion(outputs, labels)
+                        _, preds = torch.max(outputs, 1)
+
+                else:
+                    raise 
     
                 epoch_loss += loss.item()
                 epoch_corrects += torch.sum(preds == labels.data)
@@ -76,36 +85,36 @@ def train_model(net, dataloaders_dict, criterion, optimizer, num_epochs, early_s
             print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
 
             if phase == 'train':
-                train_loss_hist.append(epoch_loss)
-                train_corrects_hist.append(epoch_acc)
+                train_losses.append(epoch_loss)
+                train_accuracies.append(epoch_acc)
             else:
-                val_loss_hist.append(epoch_loss)
-                val_corrects_hist.append(epoch_acc)
+                val_losses.append(epoch_loss)
+                val_accuracies.append(epoch_acc)
 
-            if val_loss_hist[-1] < best_val_loss:
-                vest_val_loss = val_losses[-1]
-                no_improve_cnt = 0
-                if save_path is not None:
-                    state = {
-                    'model_state_dict': net.state_dict(),
-                    'optimizer_state_dict': optimizer.state_dict(),
-                    'val_loss': val_losses[-1]
-                    }
-                    torch.save(state, save_path)
-            else:
-                no_improve_cnt += 1
+                if val_losses[-1] < best_val_loss:
+                    vest_val_loss = val_losses[-1]
+                    no_improve_cnt = 0
+                    if save_path is not None:
+                        state = {
+                        'model_state_dict': net.state_dict(),
+                        'optimizer_state_dict': optimizer.state_dict(),
+                        'val_loss': val_losses[-1]
+                        }
+                        torch.save(state, save_path)
+                else:
+                    no_improve_cnt += 1
+    
+                if early_stopping and no_improve_cnt >= early_stopping:
+                    print('Stopping early')
+                    break
 
-            if early_stopping and no_improve_cnt >= early_stopping:
-                print('Stopping early')
-                break
-
-    plt.plot(train_loss_hist, label='training loss')
-    plt.plot(val_loss_hist, label='validation loss')
+    plt.plot(train_losses, label='training loss')
+    plt.plot(val_losses, label='validation loss')
     plt.legend()
     plt.show()
 
-    plt.plot(train_corrects_hist, label='training accuracy')
-    plt.plot(val_corrects_hist, label='validation accuracy')
+    plt.plot(train_accuracies, label='training accuracy')
+    plt.plot(val_accuracies, label='validation accuracy')
     plt.legend()
     plt.show()
 
